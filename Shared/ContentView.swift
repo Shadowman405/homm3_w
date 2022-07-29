1e0e150e0860c25d9f82330081e79805911599c2
m_mitin <maximus405@gmail.com>
2022-07-30 02:22:33 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Maxim Mitin on 30.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/homm3_wApp.swift
@@ -0,0 +1,17 @@
+//
+//  homm3_wApp.swift
+//  Shared
+//
+//  Created by Maxim Mitin on 30.07.22.
+//
+
+import SwiftUI
+
+@main
+struct homm3_wApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
